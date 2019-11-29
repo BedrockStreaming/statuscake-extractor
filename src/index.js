73148fd25a8e6b414fd552991dfe4b4b3b900dd4
Virgile Mino-Vercellis <virgile.mino-vercellis@m6.fr>
2019-11-29 17:38:15 +0100

    feat(server): set up config

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ const fetchStatuCakeData = require('./statuscake');
 const server = require('./server');
 const storage = require('./statuscakedata');
 
-const regex = /\s?([-])\s?/;
+const regexp = config.statuscake.regex;
 
-const getTagsFromTitle = Title => Title.split(regex);
+const getTagsFromTitle = title => title.match(new RegExp(regexp));
+const tags = [(config.statuscake.customer), (config.statuscake.service), (config.statuscake.device), (config.statuscake.version)];
 
 const loadtimeGauge = new client.Gauge({
-  name: 'frontend_pagespeed_loadtime_seconds',
+  name: (config.statuscake.nameofloadtimegauge),
   help: 'Loadtime of your page',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: (tags),
 });
 const filesizeGauge = new client.Gauge({
-  name: 'frontend_pagespeed_filesize_bytes',
+  name: (config.statuscake.nameoffilesizegauge),
   help: 'Filesize of your page',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: (tags),
 });
 const requestGauge = new client.Gauge({
-  name: 'frontend_pagespeed_request_count',
+  name: (config.statuscake.nameofrequestgauge),
   help: 'Number of request',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: (tags),
 });
 
 setInterval(() => {
@@ -30,10 +31,7 @@ setInterval(() => {
   if (data.length > 0) {
     data.forEach((myItem) => {
       const parsedTitle = getTagsFromTitle(myItem.Title);
-      const labels = {
-        customer: parsedTitle[0], service: parsedTitle[2], device: parsedTitle[4], version: parsedTitle[6],
-      };
-      const setter = (gauge, value) => gauge.set(labels, value, date);
+      const setter = (gauge, value) => gauge.set(parsedTitle.groups, value, date);
       setter(loadtimeGauge, myItem.LatestStats.loadTimeMS);
       setter(filesizeGauge, myItem.LatestStats.fileSizeKB);
       setter(requestGauge, myItem.LatestStats.Requests);
