f0540c2af7b8cb753e1d222d7ca5a8b2595d3e6b
VirgileLyon <45287046+VirgileLyon@users.noreply.github.com>
2019-12-12 11:55:22 +0100

    feat(server): set up config (#5)

--- src/index.js
+++ src/index.js
@@ -4,24 +4,26 @@ const fetchStatuCakeData = require('./statuscake');
 const server = require('./server');
 const storage = require('./statuscakedata');
 
-const regex = /\s?([-])\s?/;
+const {
+  tagsFromTestTitleRegexp, nameOfLoadTimeGauge, tags, nameOfFileSizeGauge, nameOfRequestGauge,
+} = config.statuscake;
 
-const getTagsFromTitle = Title => Title.split(regex);
+const matchTagsFromTitle = title => title.match(new RegExp(tagsFromTestTitleRegexp));
 
 const loadtimeGauge = new client.Gauge({
-  name: 'frontend_pagespeed_loadtime_seconds',
+  name: nameOfLoadTimeGauge,
   help: 'Loadtime of your page',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: tags,
 });
 const filesizeGauge = new client.Gauge({
-  name: 'frontend_pagespeed_filesize_bytes',
+  name: nameOfFileSizeGauge,
   help: 'Filesize of your page',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: tags,
 });
 const requestGauge = new client.Gauge({
-  name: 'frontend_pagespeed_request_count',
+  name: nameOfRequestGauge,
   help: 'Number of request',
-  labelNames: ['customer', 'service', 'device', 'version'],
+  labelNames: tags,
 });
 
 setInterval(() => {
@@ -29,11 +31,8 @@ setInterval(() => {
   const data = storage.getData();
   if (data.length > 0) {
     data.forEach((myItem) => {
-      const parsedTitle = getTagsFromTitle(myItem.Title);
-      const labels = {
-        customer: parsedTitle[0], service: parsedTitle[2], device: parsedTitle[4], version: parsedTitle[6],
-      };
-      const setter = (gauge, value) => gauge.set(labels, value, date);
+      const parsedTitle = matchTagsFromTitle(myItem.Title);
+      const setter = (gauge, value) => gauge.set(parsedTitle.groups, value, date);
       setter(loadtimeGauge, myItem.LatestStats.loadTimeMS);
       setter(filesizeGauge, myItem.LatestStats.fileSizeKB);
       setter(requestGauge, myItem.LatestStats.Requests);
